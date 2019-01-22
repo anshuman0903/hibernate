8bd52924605fd737c4c1b6fcccd4ac5de982c8a8
Anshumman <Anshumman@Anshuma>
2019-01-22 21:19:50 +0530

    hibernate

---
+++ src/hibernate/Test.java
@@ -0,0 +1,14 @@
+package hibernate;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s="hibernate";
+for(int i=0;i<=10;i++){
+	System.out.println(i);
+	System.out.println(s.toUpperCase());
+}
+	}
+
+}
